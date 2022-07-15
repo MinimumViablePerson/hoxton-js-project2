ab57ab4b9362cb7e17f1e61b2019352c3f23add9
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2022-07-15 15:01:41 +0100

    mama

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import './style.css'
 
-function render() {
+function render () {
   let appEl = document.querySelector('#app')
   if (!appEl) return
   appEl.textContent = ''
 
   let titleEl = document.createElement('h1')
-  titleEl.textContent = 'No, its MY project!!!'
+  titleEl.textContent = 'Is this the real life?'
 
   appEl.append(titleEl)
 }
