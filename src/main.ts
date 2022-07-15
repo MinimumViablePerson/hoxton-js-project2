f4fe5ab410eda4474492612f63d11a61f0e9cf1c
Ed 'Catboy' Putans <edputans@gmail.com>
2022-07-15 14:58:37 +0100

    Added an important change

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import './style.css'
 
-function render () {
+function render() {
   let appEl = document.querySelector('#app')
   if (!appEl) return
   appEl.textContent = ''
 
   let titleEl = document.createElement('h1')
-  titleEl.textContent = 'My project!!!'
+  titleEl.textContent = 'No, its MY project!!!'
 
   appEl.append(titleEl)
 }
